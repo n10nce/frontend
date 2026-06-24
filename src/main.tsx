5e60d6106877d9e2a0a637aa253f8d22b297213d
anishshobithps <anish.shobith19@gmail.com>
2026-06-25 00:00:29 +0530

    feat: basic clerk setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { ClerkProvider } from "@clerk/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <ClerkProvider
+        publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      >
+        <App />
+      </ClerkProvider>
     </ThemeProvider>
   </StrictMode>
 )
